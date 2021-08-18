f6d7ad6e5bc633a03fb83766b79b69cbe6dace49
tgashwinkumar <tgashwinkumar@gmail.com>
2021-08-18 08:02:02 +0530

    added bleu, formatted googleTrans, yet to work on docs

---
+++ bleu.py
@@ -0,0 +1,49 @@
+import sacrebleu
+from sacremoses import MosesDetokenizer
+
+def get_bleu_score_from_file(predict_text_file: str, test_text_file: str) -> float:
+    
+    detokenizer = MosesDetokenizer(lang='en')
+
+    refs = []
+    with open(test_text_file, 'r') as test:
+        for line in test:
+            refs.append(detokenizer.detokenize(line.strip().split()))
+    print("Test file - first sentence: ", refs[0])
+    refs = [refs]
+
+    preds = []
+    with open(predict_text_file, 'r') as pred:
+        for line in pred:
+            preds.append(detokenizer.detokenize(line.strip().split()))
+    print("Predicted file - first sentence: ", preds[0])    
+
+    bleu = sacrebleu.corpus_bleu(preds, refs)
+    return bleu.score
+
+def get_sentence_bleu_score_from_file(predict_text_file: str, test_text_file: str, bleu_evaluate_file: str) -> None:
+    
+    detokenizer = MosesDetokenizer(lang='en')
+
+    refs = []
+    with open(test_text_file, 'r') as test:
+        for line in test:
+            refs.append(detokenizer.detokenize(line.strip().split()))
+    print("Test file - first sentence: ", refs[0])
+
+    preds = []
+    with open(predict_text_file, 'r') as pred:
+        for line in pred:
+            preds.append(detokenizer.detokenize(line.strip().split()))
+    print("Predicted file - first sentence: ", preds[0])    
+
+    with open(bleu_evaluate_file, 'w+') as bleu_file:
+        for line in zip(refs, preds):
+            test = line[0]
+            pred = line[1]
+            print(test, '\t----->\t', pred)
+            bleu = sacrebleu.sentence_bleu(pred, [test], smooth_method='exp')
+            print(bleu.score, "\n")
+            bleu_file.write(str(bleu.score),'\n')
+            
+    return 
--- translate.py
+++ translate.py
@@ -4,11 +4,12 @@ from googletrans import Translator
 
 translator = Translator()
 
-f = open('VPC-Contest\Resources\Mahabharatha-Adiparva-Section22-ta.txt', 'r', encoding='utf-8')
-g = open('gtr.txt', 'w')
-s = f.read()
+# f = open('test.txt', 'r', encoding='utf-8')
+# g = open('gtr.txt', 'w')
+s = "ஓ பாவங்களற்றவரே"
 
-g.write(translator.translate(s, dest='en').text)
+print(translator.translate(s, dest='en').text)
 
-g.close()
-f.close()
+# g.close()
+# f.close()
+
