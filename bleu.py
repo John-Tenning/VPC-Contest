336c873c2b3a1674100ca0c7cecb5df824c2b60a
Suvan Sathyendira <46626983+suvanbalu@users.noreply.github.com>
2021-08-21 21:50:45 +0530

    Fixed bugs
    1. Discarding new line character
    2. Calculating average bleu_score of a file
    3. Removed clean_quote function

---
+++ bleu.py
@@ -0,0 +1,64 @@
+"""@package bleu
+Documentation for this module.
+
+More details.
+"""
+
+import sacrebleu
+from sacremoses import MosesDetokenizer
+from clean import quotes_clean
+
+def get_bleu_score_from_file(predict_text_file: str, test_text_file: str) -> float:
+    """
+    This function takes a whole text file as input and returns the BLEU score. 
+    For reference, see https://blog.machinetranslation.io/compute-bleu-score/ 
+    @param  predict_text_file   The predicted text file.
+    @param  test_text_file  The test text file.
+    @return The BLEU score in float.
+    """
+    detokenizer = MosesDetokenizer(lang='en')
+    refs = []
+    with open(test_text_file, 'r') as test:
+        for line in test:
+            refs.append(detokenizer.detokenize(line.strip().split()))
+    print("Test file - first sentence: ", refs[0])
+    refs = [refs]
+    preds = []
+    with open(predict_text_file, 'r') as pred:
+        for line in pred:
+            preds.append(detokenizer.detokenize(line.strip().split()))
+    print("Predicted file - first sentence: ", preds[0])    
+    bleu = sacrebleu.corpus_bleu(preds, refs)
+    return bleu.score
+
+def get_sentence_bleu_score_from_file(predict_text_file: str, test_text_file: str, bleu_evaluate_file: str) -> None:
+    
+    detokenizer = MosesDetokenizer(lang='en')
+
+    refs = []
+    with open(test_text_file, 'r') as test:
+        for line in test:
+            line = quotes_clean(line)
+            refs.append(detokenizer.detokenize(line.strip().split()))
+    print("Test file - first sentence: ", refs[0])
+
+    preds = []
+    with open(predict_text_file, 'r') as pred:
+        for line in pred:
+            line = quotes_clean(line)
+            preds.append(detokenizer.detokenize(line.strip().split()))
+    print("Predicted file - first sentence: ", preds[0])    
+
+    with open(bleu_evaluate_file, 'w+') as bleu_file:
+        for line in zip(refs, preds):
+            test = line[0]
+            pred = line[1]
+            print(test, '\t----->\t', pred)
+            bleu = sacrebleu.sentence_bleu(pred, [test], smooth_method='exp')
+            print(bleu.score, "\n")
+            bleu_file.write(str(bleu.score)+'\n')
+
+    return 
+
+'''for i in range(22,32) :
+  get_sentence_bleu_score_from_file(f"/content/English-{i}.txt" ,f"/content/translate-{i}.txt" ,f"/content/Results/res-{i}.txt")'''
